679c26d3f580d7eeca81df8a3721055fb5de85e1
plzletmeknow <97961088+plzletmeknow@users.noreply.github.com>
2022-04-01 14:38:05 +0900

    wetube architecture

---
+++ src/routers/userRouter.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { remove, edit } from "../controllers/userController";
+const userRouter = express.Router();
+
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
+
+export default userRouter;
